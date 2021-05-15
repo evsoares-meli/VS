616be363741ed0587b20b91bedbde57c880c1e2f
evsoares-meli <evandro.soares@mercadolivre.com>
2021-05-14 22:48:16 -0300

    updt

--- add_vlans.py
+++ add_vlans.py
@@ -92,7 +92,7 @@ class ProvisionBackbonePOP (Script):
 			for vlan in VLAN.objects.filter(site=vlan.site):
 				attrs = [
 					vlan.name,
-					vlan.sitetenant
+					vlan.tenant.name
 				]
 				output.append(','.join(attrs))
 
