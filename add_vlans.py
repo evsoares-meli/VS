6f1294dd9a2df517f5675190b2f0a30b7ef1002f
evsoares-meli <evandro.soares@mercadolivre.com>
2021-05-14 22:52:41 -0300

    alter_VID_outout

--- add_vlans.py
+++ add_vlans.py
@@ -92,7 +92,7 @@ class ProvisionBackbonePOP (Script):
 		for vlan in VLAN.objects.filter(site=vlan.site):
 			attrs = [
 				vlan.name,
-				vlan.vid.name,
+				vlan.V.name,
 				vlan.tenant.name,
 				vlan.tenant.name
 			]
